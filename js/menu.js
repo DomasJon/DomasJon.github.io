16975d402a03d488783de8113604990bafee7dec
DomasJon <jona0016@hz.nl>
2023-10-11 19:51:21 +0200

    Changed my website according from the received feedback

--- js/menu.js
+++ js/menu.js
@@ -7,3 +7,14 @@ menu_toggle.addEventListener('click', () => {
     menu_toggle.classList.toggle('is-active');
     sidebar.classList.toggle('is-active');
 })
+
+//accordion for FAQ page
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    });
+});
+    
